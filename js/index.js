f709a5602704cb2811cf02923d7c5765fae7c4d9
Fyodor <pfyodor@gmail.com>
2017-10-04 16:47:48 +0300

    new interface

--- js/index.js
+++ js/index.js
@@ -2,19 +2,58 @@
  * Created by pfyod on 03.10.2017.
  */
 function about() {
-    document.querySelector('.about').style.bottom = "-200px";
-    document.querySelector('.contacts').style.bottom = "-100vh";
-    document.querySelector('.portfolio').style.bottom = "-100vh";
+    document.querySelector('.about').style.left = "50%";
+    document.querySelector('.contacts').style.left = "-50%";
+    document.querySelector('.portfolio').style.left = "-50%";
+    document.querySelector('.menu').style.left = "-50%";
+    document.querySelector('.about').style.opacity = "1";
+    document.querySelector('.contacts').style.opacity = "0";
+    document.querySelector('.portfolio').style.opacity = "0";
+    document.querySelector('.menu').style.opacity = "0";
+    document.querySelector('nav ul li:nth-of-type(1) a').style.textDecoration = "underline";
+    document.querySelector('nav ul li:nth-of-type(2) a').style.textDecoration = "";
+    document.querySelector('nav ul li:nth-of-type(3) a').style.textDecoration = "";
 }
 
 function contacts() {
-    document.querySelector('.contacts').style.bottom = "-200px";
-    document.querySelector('.about').style.bottom = "-100vh";
-    document.querySelector('.portfolio').style.bottom = "-100vh";
+    document.querySelector('.contacts').style.left = "50%";
+    document.querySelector('.about').style.left = "-50%";
+    document.querySelector('.portfolio').style.left = "-50%";
+    document.querySelector('.menu').style.left = "-50%";
+    document.querySelector('.about').style.opacity = "0";
+    document.querySelector('.contacts').style.opacity = "1";
+    document.querySelector('.portfolio').style.opacity = "0";
+    document.querySelector('.menu').style.opacity = "0";
+    document.querySelector('nav ul li:nth-of-type(3) a').style.textDecoration = "underline";
+    document.querySelector('nav ul li:nth-of-type(1) a').style.textDecoration = "";
+    document.querySelector('nav ul li:nth-of-type(2) a').style.textDecoration = "";
 }
 
 function portfolio() {
-    document.querySelector('.portfolio').style.bottom = "-200px";
-    document.querySelector('.contacts').style.bottom = "-100vh";
-    document.querySelector('.about').style.bottom = "-100vh";
+    document.querySelector('.portfolio').style.left = "50%";
+    document.querySelector('.contacts').style.left = "-50%";
+    document.querySelector('.about').style.left = "-50%";
+    document.querySelector('.menu').style.left = "-50%";
+    document.querySelector('.about').style.opacity = "0";
+    document.querySelector('.contacts').style.opacity = "0";
+    document.querySelector('.portfolio').style.opacity = "1";
+    document.querySelector('.menu').style.opacity = "0";
+    document.querySelector('nav ul li:nth-of-type(2) a').style.textDecoration = "underline";
+    document.querySelector('nav ul li:nth-of-type(1) a').style.textDecoration = "";
+    document.querySelector('nav ul li:nth-of-type(3) a').style.textDecoration = "";
+}
+
+function menu() {
+    document.querySelector('.menu').style.left = "50%";
+    document.querySelector('.portfolio').style.left = "-50%";
+    document.querySelector('.contacts').style.left = "-50%";
+    document.querySelector('.about').style.left = "-50%";
+    document.querySelector('.menu').style.opacity = "1";
+    document.querySelector('.about').style.opacity = "0";
+    document.querySelector('.contacts').style.opacity = "0";
+    document.querySelector('.portfolio').style.opacity = "0";
+    document.querySelector('.menu').style.opacity = "1";
+    document.querySelector('nav ul li:nth-of-type(2) a').style.textDecoration = "";
+    document.querySelector('nav ul li:nth-of-type(1) a').style.textDecoration = "";
+    document.querySelector('nav ul li:nth-of-type(3) a').style.textDecoration = "";
 }
