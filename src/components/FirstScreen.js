d04d6a6c53d482cbbb7ebe57b7e0131ed8682a05
Fyodor <fyodor@MacBook-Pro-Fyodor.local>
2018-09-08 11:21:19 +0300

    fix

--- src/components/FirstScreen.js
+++ src/components/FirstScreen.js
@@ -50,8 +50,8 @@ class Info extends Component {
     for (var i = 0, max = all.length; i < max; i++) {
       const tween2 = TweenMax.from(all[i], 0.5, { y: 100, opacity: 0 });
       var myScene = new ScrollMagic.Scene({
-        triggerElement: all[i],
-        offset: -200
+        triggerElement: all[i]
+        //offset: -200
       })
         .setTween(tween2)
         .addTo(controller2);
